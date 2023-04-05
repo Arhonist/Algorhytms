7e6865f46538fc67f016a238535171a6ff4b97ec
Aushev Nikita <mr_uber@mail.ru>
2023-04-05 12:32:13 +0500

    Few JS object riddles

---
+++ javaScript/countObjValues.js
@@ -0,0 +1,20 @@
+// Напишите функцию count(obj), которая возвращает количество свойств объекта:
+
+// let user = {
+//   name: 'John',
+//   age: 30
+// };
+
+// alert( count(user) ); // 2
+// Постарайтесь сделать код как можно короче.
+
+function count(user) {
+  return Object.values(user).length;
+}
+
+let user = {
+  name: 'John',
+  age: 30,
+};
+
+console.log(count(user));
